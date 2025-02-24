8664dd0125bbaf70daf5764113c15dd35f91d1d1
ibrahem-ghaybour <138737633+ibrahem-ghaybour@users.noreply.github.com>
2025-02-24 10:43:06 +0200

    add scss and tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1 class="text-4xl text-[red]">hello</h1>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/styles/main.scss"],
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
 });
