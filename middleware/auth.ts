645b25549c7b41adc5f5e7a52582488267ed8f35
ibrahem-ghaybour <138737633+ibrahem-ghaybour@users.noreply.github.com>
2025-02-27 10:54:06 +0200

    update middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,39 +1,32 @@
 const useStorage = useAuthStore();
+
 export default defineNuxtRouteMiddleware((to, from) => {
   if (process.server) return;
-  let token = localStorage.getItem("token");
-  if (token && to.path === from.path) {
-    useStorage.token = token;
-    useStorage.fetchUserProfile(token);
-  } else if (token) {
-    useStorage.token = token;
-    // useStorage.fetchUserProfile(token);
-  }
-  if (!token) {
-    const hash = window.location.hash;
-    const tokenMatch = hash.match(/access_token=([^&]*)/);
-    token = tokenMatch ? tokenMatch[1] : null;
 
-    if (token) {
-      console.log("Extracted token from URL:", token);
+  let token = localStorage.getItem("token");
+
+  if (token) {
+    useStorage.token = token;
+    if (to.path === from.path) {
+      useStorage.fetchUserProfile(token);
+    }
+  } else {
+    const tokenMatch = window.location.hash.match(/access_token=([^&]*)/);
+    if (tokenMatch) {
+      token = tokenMatch[1];
       localStorage.setItem("token", token);
-      token = localStorage.getItem("token");
       useStorage.token = token;
+
       window.history.replaceState(null, "", window.location.pathname);
-      window.location.reload();
-      // if (from.path === "/signup" || from.path === to.path) {
-      // useStorage.fetchUserProfile(token);/
-      // }
-      console.log("Token stored in local storage:");
     }
   }
 
-  if (!token && !["/login", "/signup"].includes(to.path)) {
+  if (!useStorage.token && !["/login", "/signup"].includes(to.path)) {
     console.log("No token, redirecting to login...");
     return navigateTo("/login");
   }
 
-  if (token && ["/login", "/signup"].includes(to.path)) {
+  if (useStorage.token && ["/login", "/signup"].includes(to.path)) {
     console.log("User already authenticated, redirecting to home...");
     return navigateTo("/");
   }
